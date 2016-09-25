0f1e22e9ca4ee754c6e15e9a5d0b6314667333d4
A. Vatsaev <azero79@gmail.com>
2016-09-25 17:17:08 +0200

    using strict mode in app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const bodyParser = require('body-parser');
 
 let app = express();
