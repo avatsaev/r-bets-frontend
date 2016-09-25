509b72fc5d55b7fe93b12ab544c81a7f2f8cdbf3
A. Vatsaev <azero79@gmail.com>
2016-09-26 00:15:32 +0200

    compile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   actioncable:{
-    url: "ws://ec2-54-87-134-1.compute-1.amazonaws.com/cable"
+    url: "ws://ec2-54-166-197-95.compute-1.amazonaws.com/cable"
   },
   api:{
-    url: "http://ec2-54-87-134-1.compute-1.amazonaws.com/api/v1/"
+    url: "http://ec2-54-166-197-95.compute-1.amazonaws.com/api/v1/"
   },
   backend:{
-    url: "ec2-54-87-134-1.compute-1.amazonaws.com"
+    url: "ec2-54-210-116-35.compute-1.amazonaws.com"
   }
 };
