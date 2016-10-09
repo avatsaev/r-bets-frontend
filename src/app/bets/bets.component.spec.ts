5f25b87653ad785a824502aa3be66f656d030926
A. Vatsaev <azero79@gmail.com>
2016-10-09 12:02:16 +0200

    more bets

---
+++ src/app/bets/bets.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { BetsComponent } from './bets.component';
+
+describe('Component: Bets', () => {
+  it('should create an instance', () => {
+    let component = new BetsComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/bets/bets.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Bet } from '../bet/bet'
+import { BetService } from '../bet/bet.service'
+
+@Component({
+  selector: 'app-bets',
+  templateUrl: './bets.component.html',
+  styleUrls: ['./bets.component.sass'],
+  providers: [BetService]g
+})
+export class BetsComponent implements OnInit {
+
+  latest_bets:Array<Bet>;
+
+  constructor(private bet_service:BetService) {
+
+    this.latest_bets = new Array<Bet>();
+
+
+    this.bet_service.get_bets({last: 100}, (res)=>{
+
+      const result = res.json();
+
+      for(let json_bet of result.bets){
+        this.latest_bets.push(new Bet(json_bet));
+      }
+
+      this.latest_bets.reverse();
+
+    });
+  }
+
+  ngOnInit() {
+  }
+
+}
